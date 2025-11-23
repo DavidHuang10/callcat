af517232bc54b2f4179e3a94b7c150d53a6950ba
David Huang <david.huang@duke.edu>
2025-11-22 21:18:48 -0500

    restart backend

--- backend/src/main/java/com/callcat/backend/entity/UserPreferences.java
+++ backend/src/main/java/com/callcat/backend/entity/UserPreferences.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "user_preferences")
 public class UserPreferences {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
